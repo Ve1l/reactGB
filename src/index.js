e747dee1bc4bf5764b5c612e2b8480b4ada01b48
[ve1l] <[leve1l@yandex.ru]>
2021-05-19 13:17:10 +0300

    add Readme.md

--- src/index.js
+++ src/index.js
@@ -2,34 +2,3 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const messages = ['Hello']
-
-const sendMessage = () => {
-  messages.push('World')
-  render()
-}
-
-const Message = ({message}) => {
-  return (
-    <React.Fragment>
-      <h1>{message}</h1>
-    </React.Fragment>
-  )
-}
-
-const MessageField = ({messages}) => {
-  return (
-    <>
-    <button onClick={sendMessage}>send</button>
-    {messages.map((message, index) => (
-    <Message message={message} key={index} isVisible={true} />
-    ))}
-    </>
-  )
-}
-
-const render = () => {
-  ReactDOM.render(<MessageField messages={messages} />, document.getElementById('root'))
-}
-
-render()
